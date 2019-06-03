3560054b47b9a3ae16ef5af7f2d1cfeb3f4df955
Dmitriy Ilchenko <dilchenko@zaraffasoft.com>
2019-06-03 14:37:17 +0300

    Added popup component.

---
+++ app/components/Popup/index.js
@@ -0,0 +1,86 @@
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Keyboard,
+  LayoutAnimation,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback
+} from 'react-native'
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import styles from './styles';
+
+
+const layoutAnimationConfig = {
+  duration: 350,
+  create: { type: 'easeIn', property: 'opacity' },
+  update: { type: 'spring', springDamping: 0.8 },
+  delete: { type: 'easeIn', property: 'opacity', duration: 200 },
+};
+
+class Popup extends Component {
+
+  state = { step: 0 };
+
+  onConfirm() {
+
+  }
+
+  closeModal() {
+    this.props.onModalToggle(false);
+    LayoutAnimation.configureNext(layoutAnimationConfig)
+  }
+
+  hideOrBack = () => {
+    const { step } = this.state;
+
+    if (step !== 1) {
+      this.closeModal();
+    }
+
+    this.setState({ step: 0 });
+    LayoutAnimation.configureNext(layoutAnimationConfig)
+  }
+
+  render() {
+    const { children, visible } = this.props;
+    const { step } = this.state;
+
+    return (
+      <Modal
+        coverScreen
+        useNativeDriver
+        isVisible={visible}
+        style={styles.modal}
+        onSwipeComplete={this.hideOrBack}
+        onBackdropPress={this.hideOrBack}
+        onBackButtonPress={this.hideOrBack}
+        animationInTiming={450}
+        animationOutTiming={600}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <KeyboardAvoidingView style={styles.container} behavior='padding' enabled>
+            <TouchableOpacity
+              style={styles.icon}
+              onPress={this.hideOrBack}
+            >
+              <Icon name={step > 1 ? 'arrow-left' : 'close'} color='black' size={30} />
+            </TouchableOpacity>
+            {children}
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </Modal>
+    )
+  }
+}
+
+Popup.defaultProps = {
+  onModalToggle: () => null
+};
+
+export default Popup;
---
+++ app/components/Popup/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+  modal: {
+    margin: 0,
+    justifyContent: 'flex-end',
+  },
+  container: {
+    minHeight: 300,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    padding: 20,
+    paddingBottom: 50
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontFamily: 'OpenSans-Semibold',
+  }
+});
